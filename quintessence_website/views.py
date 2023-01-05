a12dd35f08b8559d13ea9a612ae5086c4d4a05f0
kuznetsovvdanila <kuznetsovvdanila@gmail.com>
2023-01-05 13:58:58 +0300

    specific project deleting and authentication added, styles added

--- quintessence_website/views.py
+++ quintessence_website/views.py
@@ -1,5 +1,7 @@
 import json
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
@@ -13,10 +15,6 @@ def index(request):
 
 
 def project_manager(request):
-    if request.method == 'POST':
-        if request.POST.get('action_type') == "delete":
-            Project.objects.filter(id=1).delete()
-            print('deleted')
     context = {
         'projects': Project.objects.all()
     }
@@ -24,18 +22,37 @@ def project_manager(request):
 
 
 def request_handler(request):
+    response_data = {}
+
+    def delete_project():
+        Project.objects.filter(id=request.POST.get('id')).delete()
+        return {'action_type': 'delete',
+                'status': 'succeeded'}
+
+    def auth():
+        user = authenticate(username=request.POST.get('login'), password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            return {'action_type': 'auth',
+                    'status': 'succeeded'}
+        else:
+            return {'action_type': 'auth',
+                    'status': 'failed'}
+
+    def loging_out():
+        logout(request)
+        return {'action_type': 'logout',
+                'status': 'succeeded'}
+
     if request.method == 'POST':
         if request.POST.get('action_type') == "delete":
-            Project.objects.filter(id=1).delete()
-            print('deleted')
-            response_data = {'deleted': 'true'}
+            response_data = delete_project()
+        if request.POST.get('action_type') == 'auth':
+            response_data = auth()
+        if request.POST.get('action_type') == 'logout':
+            response_data = loging_out()
 
-            return HttpResponse(
-                json.dumps(response_data),
-                content_type="application/json"
-            )
-    else:
-        return HttpResponse(
-            json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json"
-        )
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
